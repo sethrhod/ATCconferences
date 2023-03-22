b429394ca214812dedb5372830d2becba1ea25ba
sethrhod <iamsethrhodes@gmail.com>
2023-03-22 14:21:26 -0400

    added android icons and prepared for bundling

--- ios/AtlCloudConf_MobileAppTests/AtlCloudConf_MobileAppTests.m
+++ ios/AtlCloudConf_MobileAppTests/AtlCloudConf_MobileAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AtlCloudConf_MobileAppTests : XCTestCase
+@interface AtlCloudConfTests : XCTestCase
 
 @end
 
-@implementation AtlCloudConf_MobileAppTests
+@implementation AtlCloudConfTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

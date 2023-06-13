c391a7497d293bf2b57c9bd2ded8275c58c9bc99
Seth Rhodes <seth.rhodes21@gmail.com>
2023-06-13 10:29:03 -0700

    new ios folder to work with name change

--- ios/ATCconferencesTests/ATCconferencesTests.m
+++ ios/ATCconferencesTests/ATCconferencesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ATCConferencesTests : XCTestCase
+@interface ATCconferencesTests : XCTestCase
 
 @end
 
-@implementation ATCConferencesTests
+@implementation ATCconferencesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
